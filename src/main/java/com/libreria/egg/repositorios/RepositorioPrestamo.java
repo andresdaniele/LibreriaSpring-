23012c1ebec15afb2af2863ef23d6e776dcac773
Andres Daniele <andresmdaniele@gmail.com>
2021-11-01 20:43:19 -0300

    First commit ramaParte2. Start working on part two

---
+++ src/main/java/com/libreria/egg/repositorios/RepositorioPrestamo.java
@@ -0,0 +1,24 @@
+package com.libreria.egg.repositorios;
+
+import com.libreria.egg.entidades.Cliente;
+import com.libreria.egg.entidades.Libro;
+import com.libreria.egg.entidades.Prestamo;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepositorioPrestamo extends JpaRepository<Prestamo, String> {
+
+    @Query("SELECT p FROM Prestamo p WHERE p.cliente = :cliente")
+    public List<Prestamo> listarPrestamosCliente(@Param("cliente") Cliente cliente);
+
+    @Query("SELECT p FROM Prestamo p WHERE p.libro = :libro AND alta = true")
+    public List<Prestamo> listarPrestamoLibros(@Param("libro") Libro libro);
+
+    @Query("SELECT p FROM Prestamo p WHERE alta = true ORDER BY p.fechaPrestamo ASC")
+    public List<Prestamo> listarTodosLosPrestamosActivos();
+
+}
---
+++ src/main/java/com/libreria/egg/servicios/ClienteServicio.java
@@ -0,0 +1,5 @@
+package com.libreria.egg.servicios;
+
+public class ClienteServicio {
+    
+}
